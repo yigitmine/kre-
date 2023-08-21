49d6cdd60c2d3a5d1ce2b142a2340562b752f2d8
yigitmine <142795912+yigitmine@users.noreply.github.com>
2023-08-21 14:01:07 +0300

    Add files via upload

---
+++ Controllers/ButunController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cosb1.Controllers
+{
+    public class ButunController : Controller
+    {
+        // GET: Butun
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }      
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Why()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cosb1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
